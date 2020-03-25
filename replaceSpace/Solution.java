2f7f14876d635c041d41a84e0068cbcb31e6ef05
Shilin4397 <Shi_lin4397@163.com>
2020-03-25 23:36:41 +0800

    替换字符串中的空格

---
+++ replaceSpace/Solution.java
@@ -0,0 +1,29 @@
+package git.replaceSpace;
+
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: ShiLin
+ * Date:
+ * Time:
+ */
+public class Solution {
+    public String replaceSpace(StringBuffer str) {
+        if(str == null) {
+            return null;
+        }
+        StringBuilder newStr = new StringBuilder();
+        for(int i = 0;i < str.length();i++) {
+            if(str.charAt(i) == ' ') {
+                newStr.append('%');
+                newStr.append('2');
+                newStr.append('0');
+            }else {
+                newStr.append(str.charAt(i));
+            }
+        }
+        return newStr.toString();
+    }
+}
