886411c5a5745eef68ffc9938cc75253f193b14e
Shilin4397 <Shi_lin4397@163.com>
2020-04-13 18:15:48 +0800

    超长正整数相加

---
+++ longNumAdd/Main.java
@@ -0,0 +1,55 @@
+package git.longNumAdd;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: ShiLin
+ * Date:
+ * Time:
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+
+    public static String AddLongInteger(String addend, String augend) {
+
+        return null;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1;
+        while ((str1 = br.readLine()) != null) {
+            String str2 = br.readLine();
+            StringBuilder s1 = new StringBuilder(str1);
+            StringBuilder s2 = new StringBuilder(str2);
+            int len1 = s1.length();
+            int len2 = s2.length();
+            if(len1 < len2) {
+                for(int i = 0; i < len2 - len1; i++) {
+                    s1.insert(0,'0');
+                }
+            }else {
+                for (int i = 0; i < len1 - len2; i++) {
+                    s2.insert(0,'0');
+                }
+            }
+            StringBuilder s = new StringBuilder();
+            int temp = 0;
+            for(int i = s1.length()-1; i >= 0; i--) {
+                int n = s1.charAt(i)-'0';
+                int m = s2.charAt(i)-'0';
+                int sum = n + m + temp;
+                s.insert(0,sum%10);
+                temp = sum/10;
+            }
+            if(temp != 0) {
+                s.insert(0,temp);
+            }
+            System.out.println(s);
+        }
+    }
+}
